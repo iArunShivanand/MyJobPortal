8bd30835412b0d39f57605a37d71bbb0bb9f7bff
iArunShivanand <arunsmareguddi.blh@gmail.com>
2025-01-27 18:27:22 +0530

    First Commit

---
+++ MyJobPortal/src/main/java/com/arunvertex/MyJobPortal/MyJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.arunvertex.MyJobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJobPortalApplication.class, args);
+	}
+
+}
---
+++ MyJobPortal/src/test/java/com/arunvertex/MyJobPortal/MyJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arunvertex.MyJobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
